35461116e555250aa6cb702eee9636b49c9654dc
jazeymich <josephkabera42@gmail.com>
2024-06-06 15:09:40 +0300

    "Updated HeroSection and Navbar components with new styles, images, and functionality"

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
+import HeroImg from '../../images/HeroImage.png'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+import HeroBgAnimation from "../HeroBgAnimation"
+import {
+  HeroContainer,
+  HeroBg,
+  HeroLeftContainer,
+  Img,
+  HeroRightContainer,
+  HeroInnerContainer,
+  TextLoop,
+  Title,
+  Span,
+  SubTitle,
+  SocialMediaIcons,
+  SocialMediaIcon,
+  ResumeButton,
+} from './HeroStyle';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              Check Resume
+            </ResumeButton>
+          </HeroLeftContainer>
+          <HeroRightContainer id="Right">
+           <Img src={HeroImg} alt="hero-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
 };
 
 export default Hero;
